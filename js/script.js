d2e7e66a1903c3cbaf8ba99725508d8b466764ed
Alex <otostogan@yandex.ru>
2021-04-07 15:58:06 +0300

    finish page cases

--- js/script.js
+++ js/script.js
@@ -1,41 +1,4 @@
 window.addEventListener('DOMContentLoaded', ()=> {
-      //modal
-
-    const modalTrigger = document.querySelectorAll('[data-modal]'),
-          modal = document.querySelector('.modal'),
-          modalCloseBtn = document.querySelector('[data-close]');
-
-
-    
-    modalTrigger.forEach(btn =>{
-        btn.addEventListener('click', () => {
-            modal.classList.add('show');
-            modal.classList.add('fade');
-            modal.classList.remove('hide');
-            document.body.style.overflow = 'hidden';
-        });
-    });
-
-    function closeModal(){
-        modal.classList.add('hide');
-        modal.classList.remove('show');
-        modal.classList.remove('fade');
-        document.body.style.overflow = '';
-    }
-
-    modalCloseBtn.addEventListener('click', closeModal);
-    
-    modal.addEventListener('click', (e) => {
-        if(e.target == modal){
-            closeModal();
-        }
-    });
-
-    document.addEventListener('keydown', (e)=>{
-        if(e.code === "Escape" && modal.classList.contains('show')){
-            closeModal();
-        }
-    });
     //menuHamburger    
     const hamburger = document.querySelector('.header__hamberger'),
     menu = document.querySelector('.menu'),
---
+++ js/smoothscrol.js
@@ -0,0 +1,47 @@
+window.addEventListener('DOMContentLoaded', ()=> {
+        function scrollTo(element){
+        window.scroll({
+            left: 0,
+            top: element.offsetTop,
+            behavior: 'smooth'
+        });
+    }
+
+    const buttons = document.querySelectorAll('.btn-invittoyellow'),
+        landingPage = document.querySelector('#landingPage'),
+        corporateWebsite = document.querySelector('#corporateWebsite'),
+        webshop = document.querySelector('#webshop'),
+        seocase = document.querySelector('#seocase');
+
+    buttons.forEach(btn => {
+        let href = btn.getAttribute('href');
+        console.log(href);
+        btn.addEventListener('click',(e)=>{
+            e.preventDefault();
+            
+            if(btn.classList.contains('landingPage')){
+                scrollTo(landingPage);
+            }
+            if(btn.classList.contains('corporateWebsite')){
+                scrollTo(corporateWebsite);
+            }
+            if(btn.classList.contains('webshop')){
+                scrollTo(webshop);
+            }
+            if(btn.classList.contains('seocase')){
+                scrollTo(seocase);
+            }
+        });
+    });
+    
+    const mediaQuery = window.matchMedia('(max-width: 1250px)');
+    if(mediaQuery.matches){
+        $('.exemple__wrapper').slick({
+            dots: true,
+            infinite: true,
+            speed: 300,
+            prevArrow: '<button type="button" class="slick-prev"><img src="img/cases/arrows/prev.svg"></button>',
+            nextArrow: '<button type="button" class="slick-next"><img src="img/cases/arrows/next.svg"></button>',
+        });
+    }
+});
